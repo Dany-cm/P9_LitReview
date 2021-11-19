bc6f65d18d37046545c04b7af3a560b2e0705d1d
Dany CM <danycmwow@gmail.com>
2021-11-19 15:13:36 +0100

    feat: Implement basic front end design and fetch tickets(image not yet working)

--- mysite/flux/urls.py
+++ mysite/flux/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import view_index
+from .views import view_index, view_create_ticket
 
 app_name = 'flux'
 
 urlpatterns = [
     path('', view_index, name='home'),
+    path('create/ticket', view_create_ticket, name='create_ticket'),
 ]
--- mysite/flux/views.py
+++ mysite/flux/views.py
@@ -1,8 +1,31 @@
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from itertools import chain
 
+from django.contrib.auth.decorators import login_required
+from django.db.models import Value, CharField
+from django.shortcuts import render, redirect
 
 # Create your views here.
-@login_required
+from .models import Ticket
+
+
 def view_index(request):
-    return render(request, 'home.html')
+    all_tickets = Ticket.objects.all()
+    all_tickets = all_tickets.annotate(content_type=Value('TICKET', CharField()))
+
+    ticket = sorted(chain(all_tickets), key=lambda instance: instance.time_created, reverse=True)
+    return render(request, 'home.html', {'tickets': ticket})
+
+
+@login_required
+def view_create_ticket(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        image = request.FILES.get('image')
+
+        ticket = Ticket(title=title, description=description, image=image, user=request.user)
+        ticket.save()
+
+        return redirect('flux:home')
+    else:
+        return render(request, 'create_ticket.html')
